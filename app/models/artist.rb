6afac7c8004302a784645c384a2dbb4c578b299a
tiburon <thechanmoon@gmail.com>
2020-02-24 01:52:25 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,49 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    @@all.inject(0) do |sum, artist|
+      sum + artist.years_experience
+    end
+  end
+
+  def self.most_prolific 
+    (@@all.map do |artist| 
+      (artist.paintings.length.to_f/artist.years_experience.to_f)
+    end).max
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,48 @@
 class Gallery
 
-  attr_reader :name, :city
-
+  attr_reader :name, :city, :artist, :painting
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+  def artists
+    paintings.map do |painting|
+      painting.artist
+    end
+  end
+
+  def artist_names
+    artists.map do |artist|
+      artist.name
+    end
+  end
+
+  def most_expensive_painting
+    (paintings.map do |painting| painting.price end).max
+  end
 end
+
+# a = Artist.new("Chan", 15)
+# a1 = Artist.new("Jay", 15)
+# g = Gallery.new("New York Gallery","New York")
+# g1 = Gallery.new("Seoul Gallery","Seoul")
+# p = Painting.new("Tibuorn", 1000, a, g)
+# p1 = Painting.new("Special", 10000, a1, g1)
+# p2 = Painting.new("Special Jay", 20000, a1, g1)
+# 50.times do p2 = Painting.new("Special Jay", 20000, a1, g1) end
+# a.paintings
+# a.galleries
+# a.cities
